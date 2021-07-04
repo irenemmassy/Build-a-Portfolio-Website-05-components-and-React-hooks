52b232b19015133deaea865cf929939076353326
minah-mmassy <frontendkonki@gmail.com>
2021-07-04 21:27:25 +0300

    first commit

---
+++ servicesData.js
@@ -0,0 +1,221 @@
+const Services = {
+  services: [
+    {
+      id: "1",
+      image: "https://image.flaticon.com/icons/svg/426/426487.svg",
+      title: "Web Development",
+      description:
+        "Expand your business outreach by capitalizing on web development services.",
+    },
+    {
+      id: "2",
+      image: "/images/img2.svg",
+      title: "Mobile Development",
+      description:
+        "Optimize smart ways to deal with the customers.Hire skilled mobile app developer",
+    },
+    {
+      id: "3",
+      image: "/images/img3.png",
+      title: "PWA development",
+      description:
+        "Progressive Web Apps, it can be said that this is a new technology for interacting with the target audience.",
+    },
+    {
+      id: "4",
+      image: "/images/img6.svg",
+      title: "Cross-Platform App",
+      description:
+        "Using React Native and flutter to build single app that performs on both Android and iOS.",
+    },
+    {
+      id: "5",
+      image: "/images/img4.svg",
+      title: "UX/UI Designs",
+      description:
+        "intuitive, user-friendly products for the success of your business to create a human-friendly visual language.",
+    },
+    {
+      id: "6",
+      image: "/images/img5.svg",
+      title: "Digital Marketing",
+      description:
+        "Data-driven strategies maximize lifecycle value by align your technology and marketing execution.",
+    },
+  ],
+  Industry: [
+    {
+      id: "1",
+      image: "/images/img12.svg",
+      title: "Construction",
+      description:
+        "Advanced software solutions that take Construction into the Digital Era.",
+    },
+    {
+      id: "2",
+      image: "/images/img13.svg",
+      title: "Healthcare",
+      description:
+        "Patient-friendly software that empowers healthcare industry workers.",
+    },
+    {
+      id: "3",
+      image: "/images/img14.svg",
+      title: "Retail & ECommerce ",
+      description:
+        "Engaging and exciting software solutions for modern retail.",
+    },
+    {
+      id: "4",
+      image: "/images/img15.svg",
+      title: "FinTech",
+      description:
+        "Meet the demands of modern customers in speed and security with scalable financial technology",
+    },
+    {
+      id: "5",
+      image: "/images/img16.svg",
+      title: "Travel & Hospitality",
+      description:
+        "Extend the comfort of your resort with practical software solutions.",
+    },
+    {
+      id: "6",
+      image: "/images/img17.svg",
+      title: "Logistics",
+      description:
+        "Use all the latest tech available to make your logistics run like clockwork.",
+    },
+  ],
+  Technology: [
+    {
+      id: "1",
+      image: "/images/img7.svg",
+      tile: "Mobile Development",
+      techs: [
+        {
+          id: "1",
+          name: "Flutter Framework",
+        },
+        {
+          id: "2",
+          name: "Android Apps",
+        },
+        {
+          id: "3",
+          name: "React Native",
+        },
+        {
+          id: "4",
+          name: "Ionic Framework",
+        },
+        {
+          id: "5",
+          name: "Xamarin Framework",
+        },
+      ],
+    },
+    {
+      id: "2",
+      image: "/images/img8.svg",
+      tile: "UX/UI Designs",
+      techs: [
+        {
+          id: "1",
+          name: "Sketch",
+        },
+        {
+          id: "2",
+          name: "Figma",
+        },
+        {
+          id: "3",
+          name: "Flinto",
+        },
+        {
+          id: "4",
+          name: "Adobe XD",
+        },
+        {
+          id: "5",
+          name: "Blender",
+        },
+      ],
+    },
+    {
+      id: "3",
+      image: "/images/img9.svg",
+      tile: "Web Development",
+      techs: [
+        {
+          id: "1",
+          name: "HTML",
+        },
+        {
+          id: "2",
+          name: "CSS",
+        },
+        {
+          id: "3",
+          name: "ReactJs",
+        },
+        {
+          id: "4",
+          name: "Jquery",
+        },
+        {
+          id: "5",
+          name: "JSON",
+        },
+      ],
+    },
+    {
+      id: "4",
+      image: "/images/img10.svg",
+      tile: "E-commerce",
+      techs: [
+        {
+          id: "1",
+          name: "Shopify",
+        },
+        {
+          id: "2",
+          name: "WooCommerce",
+        },
+        {
+          id: "3",
+          name: "Drubal",
+        },
+        {
+          id: "4",
+          name: "Magento",
+        },
+      ],
+    },
+    {
+      id: "5",
+      image: "/images/img11.svg",
+      tile: "Server Side",
+      techs: [
+        {
+          id: "1",
+          name: "Node.js",
+        },
+        {
+          id: "2",
+          name: "PHP",
+        },
+        {
+          id: "3",
+          name: "Express.js Framework",
+        },
+        {
+          id: "4",
+          name: "Laravel Framework",
+        },
+      ],
+    },
+  ],
+};
+
+export default Services;
